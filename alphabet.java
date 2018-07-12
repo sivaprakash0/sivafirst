4f82909d67ad78ed67e53ea6cabb76c2f088b62b
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-12 15:47:00 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,7 @@
+public class alphabet {
+public static void main(String[] args) {
+	char c=args[0].charAt(0);
+	if(c>='A' && c<='Z'||c>='a' && c<='z'){System.out.println("Alphabet");}
+	else{System.out.println("Not");}
+}
+}
