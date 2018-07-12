37b2d861840375a11ec27188b5a7ea6f6edecf3e
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-12 17:48:15 +0530

    Add files via upload

---
+++ SumArrt.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumArrt {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int no=in.nextInt();
+	int no1=in.nextInt();
+	int num[]=new int[no];
+	int add=0,ind=0;
+	for(int i=0;i<no;i++){
+		num[i]=in.nextInt();
+		if(num[i]==no1){
+		ind=i;
+		}
+	}
+	int ad=num[0];
+	for(int j=1;j<=ind;j++){
+		ad=ad+num[j];
+	}
+	System.out.println(ad);
+}
+}
