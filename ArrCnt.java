8772bd6039d1defb091950665c4c035490c95b82
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-12 17:57:31 +0530

    Add files via upload

---
+++ ArrCnt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ArrCnt {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int num=in.nextInt();
+	String s=Integer.toString(num);
+	System.out.println(s.length());
+}
+}
