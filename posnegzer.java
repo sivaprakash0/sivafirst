b43551c082a4d6b17a5d72a25827fe8ba95d8605
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-12 17:57:08 +0530

    Delete posnegzer.java

--- posnegzer.java
+++
@@ -1,9 +0,0 @@
-
-public class pos {
-public static void main(String args[]) {
-	int n=Integer.parseInt(args[0]);
-	if(n>0&&n<=100000)System.out.println("positive");
-	  else if(n==0)System.out.println("zero");
-	  else System.out.println("negative");
-}
-}
