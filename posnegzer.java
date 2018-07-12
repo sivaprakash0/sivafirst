d26f5c1f835732e0a06b70965c0c1462baad5bae
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-12 10:18:42 +0530

    Add files via upload

---
+++ posnegzer.java
@@ -0,0 +1,9 @@
+
+public class pos {
+public static void main(String args[]) {
+	int n=Integer.parseInt(args[0]);
+	if(n<0){System.out.println("negative");}
+	  else if(n==0){System.out.println("zero");}
+	  else{System.out.println("positive");}	
+}
+}
